63c6d6d1b108d158a5301562f11a86adc4c43811
aliemili <aliemili2013@yahoo.com>
2021-04-20 21:45:35 +0430

    add prod and dev configs

--- webpack.common.js
+++ webpack.common.js
@@ -1,16 +1,8 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-  mode: "development",
-  //   devltool: "none",
-  //   make it more readable
   entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    // filename: "main.[contentHash].js",
-    // by using contenthash, everytime it creates a new main.js with hashed name. the problem is that the previous file is not deleted, so we use plugins to do it
-    path: path.resolve(__dirname, "dist"),
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
